fb28ff8bb7be7813c623cda05ff238a1d423b379
e1165829 <e1165829@USSADL0099>
2016-10-13 15:45:46 -0400

    Add project files.

---
+++ OrderEntry/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompanyA.Orders;
+
+namespace CompanyA.OrderEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Product Id");
+            string id = Console.ReadLine();
+            Console.WriteLine("The Product Id is {0}", OrderValidation.IsProductValid(id) ? "valid" : "invalid");
+        }
+    }
+}
